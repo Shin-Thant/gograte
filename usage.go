698dd58daada86069c402acfd77b5b66b00c5abf
Shin-Thant <shinthant10684@gmail.com>
2024-03-02 12:36:53 +0630

    Update usage message

--- usage.go
+++ usage.go
@@ -8,7 +8,20 @@ gograte init
 gograte create [migration_name]
 
 // Migrate
-gograte migrate [db_driver] [db_url] [migrate_action]`
+gograte migrate [db_driver] [db_url] [migrate_action]
+
+// Status
+gograte status [db_driver] [db_url]
+
+// Examples
+gograte status postgres "postgres://localhost:5432/db"
+
+gograte migrate postgres "postgres://localhost:5432/db" up
+gograte migrate postgres "postgres://localhost:5432/db" down
+gograte migrate postgres "postgres://localhost:5432/db" up-one
+gograte migrate postgres "postgres://localhost:5432/db" up-to 20240228042140
+gograte migrate postgres "postgres://localhost:5432/db" down-one
+gograte migrate postgres "postgres://localhost:5432/db" down-to 20240228042140`
 
 var initUsage = `No migration directory found.
 Please run "gograte init" to create a migrations directory.`
