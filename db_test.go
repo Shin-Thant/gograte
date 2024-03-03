86e9d3325c98ac38845e44a7fc452f257dec1b2c
Shin-Thant <shinthant10684@gmail.com>
2024-03-03 15:55:54 +0630

    test(db): unit test migration table create

--- db_test.go
+++ db_test.go
@@ -1,9 +1,15 @@
 package gograte_test
 
 import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/Shin-Thant/gograte"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 func TestGetSQLDriver(t *testing.T) {
@@ -11,5 +17,39 @@ func TestGetSQLDriver(t *testing.T) {
 	mappedDriver := gograte.GetSQLDriver(inputDriver)
 	if mappedDriver != "" {
 		t.Error("Result should be empty string.")
+	}
+}
+
+func TestCreateMigrationTableIfNotExist(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	if DATABASE_URL == "" {
+		log.Fatalln("DATABASE_URL is not set.")
+	}
+
+	// setup database connection
+	db, err := sql.Open("pgx", DATABASE_URL)
+	if err != nil {
+		log.Fatalf("DB test: Error opening database: %v\n", err)
+	}
+	defer db.Close()
+
+	conn, err := db.Conn(context.Background())
+	if err != nil {
+		log.Fatalf("DB test: Error database connection: %v\n", err)
+	}
+	defer conn.Close()
+
+	_, err = gograte.CreateMigrationTableIfNotExist(db)
+	if err != nil {
+		t.Errorf("Should not throw error when creating migration table: %v", err)
+	}
+
+	_, err = gograte.CreateMigrationTableIfNotExist(db)
+	if err != nil {
+		t.Errorf("Should not throw error when creating migration table: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 require (
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.5.3
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
